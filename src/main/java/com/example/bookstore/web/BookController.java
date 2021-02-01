d29cc40b012bf829936af455c9f0293607acd007
Benjamin <joel.uusitalo@gmail.com>
2021-02-01 22:20:27 +0200

    Initial commit

---
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -0,0 +1,16 @@
+package com.example.bookstore.web;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class BookController {
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String greeting(@RequestParam(name="title") String title, 
+			@RequestParam(name ="author") String author,@RequestParam(name="year") String year, 
+			@RequestParam(name ="isbn") String isbn,@RequestParam(name ="price") String price, Model book) {
+		return "index";
+	}
+
+}
